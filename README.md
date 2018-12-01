49609077ade1fa960a949647152638e545586acd
Prince Avila_b <43385846+TennisSprinterkb@users.noreply.github.com>
2018-12-01 17:34:38 -0500

    speedCollector

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # unit-4-game2
-crystal collecter style game with a twist
-A need for speed type of game where you have to match the speed correctly with the speed limit sign!
+Crystal collecter style game with a twist
+A need for speed type with cars the game where you have to match the speed correctly with the speed limit sign!
+
+
+The player will have to guess the answer, just like in Word Guess. This time, though, the player will guess with numbers instead of letters. 
+Here's how the app works:
+
+
+
+There will be four vehicles displayed as buttons on the page.
+The player will be shown a random number(speed) at the start of the game.
+
+When the player clicks on a vehicle, it will add speed to the player's total score. 
+
+
+Your game will hide this amount until the player clicks a vehicle.
+When they do click one, update the player's speed.
+
+
+The player wins if their total score matches the random speed from the beginning of the game.
+The player loses if their score goes above the posted speed.
+
+The game restarts whenever the player wins or loses.
+
+
+When the game begins again, the player should see a new spped limit sign. Also, all the vehicles will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+
+The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,108 @@
+//Lets page load quicker
 $(document).ready(function() {
+
+    //Tracking wins & losses
+    var win = 0;
+    var loss = 0;
+
+    //Computer random speed to hit
+    var target = [];
+
+    //Random speed assigned to vehicles
+    var BentleyNum;
+    var RangeNum;
+    var GTRNum;
+    var PorscheNum;
+
+    //User score counter
+    var counter = 0;
+
+    //Game Start
+    function gameStart() {
+        var carNum = Math.floor(Math.random() * 220) + 16;
+        target.push(carNum);
+        $("#random").text(target[0]);
+        console.log("Computer number: " + target);
+
+        //Random number for vehicles
+        BentleyNum = Math.floor(Math.random() * 15) + 2;
+        console.log("Bentley: " + BentleyNum);
+
+        RangeNum = Math.floor(Math.random() * 12) + 1;
+        console.log("Range: " + RangeNum);
+
+        GTRNum = Math.floor(Math.random() * 25) +2;
+        console.log("GTR: " + GTRNum);
+
+        PorscheNum = Math.floor(Math.random() * 22) +1;
+        console.log("Porsche: " + PorscheNum);
+    };
+
+    //Reset game
+    function newGame() {
+        target.length = 0;
+        counter = 0;
+        gameStart();
+    };
+
+    //Function if player counter</=/> computer's number
+    function checkNum() {
+        if (counter === target[0]) {
+            win++;
+            $("#outcome").html("You won!!");
+            $("#win").html("Wins: "+ win);
+            //console.log("You won!!");
+            newGame();
+        }
+
+        else if (counter > target[0]) {
+            loss++;
+            $("#outcome").html("You lost!!");
+            // $(".winRlose").text("Loser");
+            $("#loss").html("Losses: "+ loss);
+            //console.log("You lost!!");
+            newGame();
+        }
+
+      
+    };
+
+    //Game start
+    gameStart();
+
+    //Clicking vehicles
+    $("#Bentley").on("click", function() {
+        counter = BentleyNum + counter;
+        $("#yourSpeed").text(counter);
+        //console.log(counter);
+        checkNum();
+    });
+
+    $("#Range").on("click", function() {
+        counter = RangeNum + counter;
+        $("#yourSpeed").text(counter);
+        checkNum();
+    });
+    $("#GTR").on("click", function() {
+        counter = GTRNum + counter;
+        $("#yourSpeed").text(counter);
+        checkNum();
     
-})
+    });
+    $("#Porsche").on("click", function() {
+        counter = PorscheNum + counter;
+        $("#yourSpeed").text(counter);
+        checkNum();
+    });
+
+
+
+    
+
+    
+    
+
+
+    
+
+});
